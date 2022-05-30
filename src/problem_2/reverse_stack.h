4bcf177fcf5890933587d0493ab1c8a8d246d72f
noaur <noau2@uw.edu>
2022-05-29 22:00:32 -0700

    Problem 1 + 2

--- src/problem_2/reverse_stack.h
+++ src/problem_2/reverse_stack.h
@@ -2,7 +2,29 @@
 
 template<typename T>
 void reverse_stack_recursively(Stack<T> &stack) {
-    /*
-     * TODO: homework
-     */
+
+    if(stack.is_empty()){
+        return;
+    }
+    else{
+        T popped = stack.peek();
+        stack.pop();
+        reverse_stack_recursively(stack);
+
+        helper_reverse(stack, popped);
+    }
+
+}
+template<typename T>
+void helper_reverse(Stack<T> &stack, T lastPop){
+    if(stack.is_empty()){
+        stack.push(lastPop);
+    }
+    else{
+        T popped = stack.peek();
+        stack.pop();
+        helper_reverse(stack, lastPop);
+        stack.push(popped);
+    }
+
 }
